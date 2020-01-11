c147296b6cf46a8ad8037c4b8451ece1a2268ce8
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-11 23:49:31 +0530

    sotre

---
+++ src/store/reducers/mealsReducer.js
@@ -0,0 +1,16 @@
+import { MEALS } from '../../../data/dummy-data'
+const initialState={
+    meals : MEALS,
+    filteredMeals: MEALS,
+    favoriteMeals: [] 
+}
+const mealsReducer = ( state={ initialState }, action ) => {
+    // switch( action.type ) {
+    //     case :
+    //         return
+    //     default:
+    //         return
+    // }
+    return state
+}
+export default mealsReducer;

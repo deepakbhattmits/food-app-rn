7aaf5d69ee209b6e35e8f312ebab9f4c101c3130
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-12 18:54:37 +0530

    refector

--- src/store/reducers/mealsReducer.js
+++ src/store/reducers/mealsReducer.js
@@ -1,16 +1,18 @@
-import { MEALS } from '../../../data/dummy-data'
-const initialState={
-    meals : MEALS,
-    filteredMeals: MEALS,
-    favoriteMeals: [] 
-}
-const mealsReducer = ( state={ initialState }, action ) => {
-    // switch( action.type ) {
-    //     case :
-    //         return
-    //     default:
-    //         return
-    // }
-    return state
-}
+/** @format */
+
+import { MEALS } from '../../../data/dummy-data';
+const initialState = {
+	meals: MEALS,
+	filteredMeals: MEALS,
+	favoriteMeals: []
+};
+const mealsReducer = (state = { initialState }, action) => {
+	// switch( action.type ) {
+	//     case :
+	//         return
+	//     default:
+	//         return
+	// }
+	return state;
+};
 export default mealsReducer;

9c6cf988c2b758d3b90e4248b90b4120b82a51d7
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-12 18:56:00 +0530

    refector

--- src/screens/CategoriesScreen.js
+++ src/screens/CategoriesScreen.js
@@ -1,7 +1,7 @@
+/** @format */
+
 import React from 'react';
-import {
-      FlatList,
-} from 'react-native';
+import { FlatList } from 'react-native';
 
 import { CATEGORIES } from '../../data/dummy-data';
 import CategoryGridTile from '../components/CategoryGridTile';
@@ -9,47 +9,48 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import CustomHeaderButton from '../components/CustomHeaderButton';
 
 const CategoriesScreen = props => {
-  const renderGridItem = itemData => {
-    return (
-     <CategoryGridTile 
-        color={ itemData.item.color }
-        title={ itemData.item.title }
-        onPress={() => {
-            props.navigation.navigate({
-            routeName: 'CategoryMeals',
-            params: {
-                categoryId: itemData.item.id
-            }
-            });
-        }}
-     />
-    );
-  };
+	console.log('TEST :', props);
+	const renderGridItem = itemData => {
+		return (
+			<CategoryGridTile
+				color={itemData.item.color}
+				title={itemData.item.title}
+				onPress={() => {
+					props.navigation.navigate({
+						routeName: 'CategoryMeals',
+						params: {
+							categoryId: itemData.item.id
+						}
+					});
+				}}
+			/>
+		);
+	};
 
-  return (
-    <FlatList
-      keyExtractor={(item, index) => item.id}
-      data={CATEGORIES}
-      renderItem={renderGridItem}
-      numColumns={2}
-    />
-  );
+	return (
+		<FlatList
+			keyExtractor={(item, index) => item.id}
+			data={CATEGORIES}
+			renderItem={renderGridItem}
+			numColumns={2}
+		/>
+	);
 };
 CategoriesScreen.navigationOptions = navData => {
-  return {
-    headerTitle: 'Meal Categories',
-    headerLeft: (
-      <HeaderButtons HeaderButtonComponent={ CustomHeaderButton }>
-        <Item
-          title="Menu"
-          iconName="ios-menu"
-          onPress={() => {
-            navData.navigation.toggleDrawer();
-          }}
-        />
-      </HeaderButtons>
-    )
-  };
+	return {
+		headerTitle: 'Meal Categories',
+		headerLeft: (
+			<HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+				<Item
+					title='Menu'
+					iconName='ios-menu'
+					onPress={() => {
+						navData.navigation.toggleDrawer();
+					}}
+				/>
+			</HeaderButtons>
+		)
+	};
 };
 
 export default CategoriesScreen;

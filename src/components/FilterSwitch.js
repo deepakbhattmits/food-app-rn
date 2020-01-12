67a7a1c92955b8a4baa9e8878b4eba1032815573
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-12 18:55:34 +0530

    refector

--- src/components/FilterSwitch.js
+++ src/components/FilterSwitch.js
@@ -1,26 +1,29 @@
+/** @format */
+
 import React from 'react';
 import { View, Text, Switch, Platform, StyleSheet } from 'react-native';
 import Colors from '../constants/Colors';
 const FilterSwitch = props => {
-    return( 
-        <View style={ styles.filterContainer }>
-               <Text>{ props.title }</Text>
-                <Switch
-                   trackColor={ { true: Colors.primaryColor } }
-                   thumbColor={ Platform.OS === 'android' ? Colors.primaryColor : ''}
-                   value={ props.state }
-                   onValueChange={ props.onChange }
-                />   
-           </View> 
-    )
+	console.log('test : ', props);
+	return (
+		<View style={styles.filterContainer}>
+			<Text>{props.title}</Text>
+			<Switch
+				trackColor={{ true: Colors.primaryColor }}
+				thumbColor={Platform.OS === 'android' ? Colors.primaryColor : ''}
+				value={props.state}
+				onValueChange={props.onChange}
+			/>
+		</View>
+	);
 };
-const styles= StyleSheet.create({
-    filterContainer: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '80%',
-        marginVertical: 15,
-        alignItems: 'center',
-    },
+const styles = StyleSheet.create({
+	filterContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		width: '80%',
+		marginVertical: 15,
+		alignItems: 'center'
+	}
 });
 export default FilterSwitch;

877853be6148d0313c7a3c91fe310edb4417fde4
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-12 18:55:27 +0530

    refector

--- src/components/DefaultText.js
+++ src/components/DefaultText.js
@@ -1,17 +1,18 @@
+/** @format */
+
 import React from 'react';
 import { Text, StyleSheet } from 'react-native';
 
 const DefaultText = props => {
-    return (
-        <Text style={ { ...styles.title, ...props.style } }>
-                { props.children }
-        </Text>
-    );
-}
-const styles= StyleSheet.create({
-    title:{
-        fontFamily: 'open-sans',
-        fontSize: 15,
-    }
+	console.log('test : ', props);
+	return (
+		<Text style={{ ...styles.title, ...props.style }}>{props.children}</Text>
+	);
+};
+const styles = StyleSheet.create({
+	title: {
+		fontFamily: 'open-sans',
+		fontSize: 15
+	}
 });
 export default DefaultText;

ef7cc9d2f3a13ec4d062d5b7517cae5adae9ab63
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-12 18:55:41 +0530

    refector

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,23 +1,24 @@
+/** @format */
+
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import DefaultText from './DefaultText';
 
-const ListItem = props => { 
-    return (
-        <View style={ styles.list }>
-            <DefaultText>{ props.children }</DefaultText>
-        </View>
-    )
-
+const ListItem = props => {
+	console.log('TEST :', props);
+	return (
+		<View style={styles.list}>
+			<DefaultText>{props.children}</DefaultText>
+		</View>
+	);
 };
-const styles= StyleSheet.create({
-    list: {
-        marginHorizontal: 5,
-        padding: 10,
-        marginVertical: 10,
-        borderColor: '#ccc',
-        borderWidth: 2,
-    },
+const styles = StyleSheet.create({
+	list: {
+		marginHorizontal: 5,
+		padding: 10,
+		marginVertical: 10,
+		borderColor: '#ccc',
+		borderWidth: 2
+	}
 });
 export default ListItem;
-

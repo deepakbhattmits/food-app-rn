db1d9267365c410aaeea7bab35807f9c5c6602e5
deepak bhatt <deepakbhattmits@gmail.com>
2020-01-15 20:43:22 +0530

    refector

--- src/components/MealList.js
+++ src/components/MealList.js
@@ -1,46 +1,48 @@
+/** @format */
+
 import React from 'react';
 import { View, FlatList, StyleSheet } from 'react-native';
 import MealItem from './MealItem';
 
 const MealList = props => {
-    console.log('test : ', props)
-    const renderMealItem = itemData => {
-        return (
-            <MealItem
-                title={ itemData.item.title }
-                image={ itemData.item.imageUrl }
-                affordability={ itemData.item.affordability }
-                complexity={ itemData.item.complexity }
-                duration={ itemData.item.duration }
-                onSelectMeal={() => {
-                        props.navigation.navigate({
-                        routeName: 'CategoryDetail',
-                        params: {
-                            mealId: itemData.item.id,
-                            mealTitle: itemData.item.title
-                        }
-                    });
-                }}
-            />
-        );
-    } 
-    return (
-        <View style={ styles.list }>
-           <FlatList 
-                data={ props.listData }
-                keyExtractor={ ( item, index ) => item.id }
-                renderItem={ renderMealItem }
-                style={ { width: '100%' }}
-            />
-       </View>
-    )
+	// console.log('test : ', props)
+	const renderMealItem = itemData => {
+		return (
+			<MealItem
+				title={itemData.item.title}
+				image={itemData.item.imageUrl}
+				affordability={itemData.item.affordability}
+				complexity={itemData.item.complexity}
+				duration={itemData.item.duration}
+				onSelectMeal={() => {
+					props.navigation.navigate({
+						routeName: 'CategoryDetail',
+						params: {
+							mealId: itemData.item.id,
+							mealTitle: itemData.item.title
+						}
+					});
+				}}
+			/>
+		);
+	};
+	return (
+		<View style={styles.list}>
+			<FlatList
+				data={props.listData}
+				keyExtractor={(item, index) => item.id}
+				renderItem={renderMealItem}
+				style={{ width: '100%' }}
+			/>
+		</View>
+	);
 };
-const styles= StyleSheet.create({
-    list: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        padding: 15,
-    }
+const styles = StyleSheet.create({
+	list: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		padding: 15
+	}
 });
-export default MealList; 
+export default MealList;
